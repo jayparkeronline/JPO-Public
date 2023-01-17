e3322f0b5e07bcad142b002e7467df9af5f881a2
Jay Parker <jayparker@jayparkeronline.com>
2023-01-17 18:04:59 -0500

    first commit

---
+++ AD Management PS Samples/add-adds-orgunit.ps1
@@ -0,0 +1,3 @@
+# Microsoft KB: https://learn.microsoft.com/en-us/powershell/module/activedirectory/new-adorganizationalunit?view=windowsserver2022-ps
+#replace <> brackets with your domain information.
+New-ADOrganizationalUnit -Name "<OU Here>" -Path "DC=<domain>,DC=<top level"
