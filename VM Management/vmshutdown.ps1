c92c8ac42f42db4be565c306397f63e789b5111f
Jay Parker <jayparker@jayparkeronline.com>
2023-01-17 19:23:40 -0500

    Updated code to share publicly

---
+++ VM Management/vmshutdown.ps1
@@ -0,0 +1,14 @@
+﻿#Author Jay Parker
+#Purpose: automate VM shutdown for hyper-v host patching
+
+
+#Create paramater and seed with hyper-v hostname
+[cmdletbinding()]
+Param($vmhost = '<server hostname>')
+#create varaiable that checks for running VMs
+$runningVM = Get-VM -ComputerName $vmhost| where state -eq 'running'
+
+# Execute loop to shutdown running VMs
+foreach ($cn in $runningVM) {
+Stop-VM $cn.name -asjob
+}
